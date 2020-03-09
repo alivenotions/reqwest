1ac2f24c65ecb8b8d5a57ed2f2d752ee95deb16c
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2020-03-10 00:53:12 +0100

    add options to fetch

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,29 @@
-type httpVerbs = 
-    'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'HEAD'
+type httpVerbs = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'HEAD'
 
-const instance = (fetch: typeof window.fetch) => (verb: httpVerbs) => (url: string, body?: any) => fetch(url, {
+const isHeadOrGet = (verb: httpVerbs) => verb === 'HEAD' || verb === 'GET'
+
+const instance = (fetch: typeof window.fetch) => (verb: httpVerbs) => (
+  resource: string,
+  body?: RequestInit['body'],
+  init?: Omit<RequestInit, 'body'>
+) => {
+  const _init = {
     method: verb,
-    body: JSON.stringify(body),
-})
+    ...(body && isHeadOrGet(verb) && { body: JSON.stringify(body) }),
+    ...(init && init),
+  }
+  return fetch(resource, _init)
+}
 
-const defaultInstance = instance(window.fetch);
+const defaultInstance = instance(window.fetch)
 
 export default {
-    get: defaultInstance('GET'),
-    post: defaultInstance('POST'),
-    put: defaultInstance('PUT'),
-    delete: defaultInstance('DELETE'),
-    patch: defaultInstance('PATCH'),
-    head: defaultInstance('HEAD'),
+  get: defaultInstance('GET'),
+  post: defaultInstance('POST'),
+  put: defaultInstance('PUT'),
+  delete: defaultInstance('DELETE'),
+  patch: defaultInstance('PATCH'),
+  head: defaultInstance('HEAD'),
 }
 
 /*
