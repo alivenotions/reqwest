b806857907a551fb0f43e6251d8a071e4dee9d13
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2020-03-10 11:07:55 +0100

    add helpers for getting the response value out
    Adding json, blob, text et al. metthods to the response object;
    this allows us to get the value without 'await'-ing the response again.

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,38 @@ const isHeadOrGet = (verb: httpVerbs) => verb === 'HEAD' || verb === 'GET'
 // TODO: add a timeout
 // TODO: interceptors
 
+const bodyTransform = [
+  'arrayBuffer',
+  'blob',
+  'formData',
+  'json',
+  'text',
+] as const
+
+interface FetchyResponse extends Response {
+  arrayBuffer(): Promise<ArrayBuffer>
+  blob(): Promise<Blob>
+  formData(): Promise<FormData>
+  json(): Promise<JSON>
+  text(): Promise<string>
+}
+
 const instance = (fetch: typeof window.fetch) => (verb: httpVerbs) => async (
   resource: string,
   body?: RequestInit['body'],
   init?: Omit<RequestInit, 'body'>
-) => {
-  const _init = {
+): Promise<FetchyResponse> => {
+  const _init: RequestInit = {
     method: verb,
     ...(body && isHeadOrGet(verb) && { body: JSON.stringify(body) }),
     ...(init && init),
   }
-  const response = await fetch(resource, _init)
-  return response
+  const _response = await fetch(resource, _init)
+  let result: FetchyResponse = _response
+  for (const methods of bodyTransform) {
+    result[methods] = await _response[methods]()
+  }
+  return result
 }
 
 const defaultInstance = instance(fetch)
@@ -42,5 +62,5 @@ export default {
     6. req.delete
     7. req.createInstance(baseurl, interceptors, timeout)
     8. req.extendInstance(baseurl, interceptors, timeout)
-    9. return cancel to kill it
+    9. return cancel to kill it?
  */
