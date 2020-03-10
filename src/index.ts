106fb7f0a9f37edaea66eb3b760563031b2a7a3d
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2020-03-10 14:37:00 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -31,43 +31,75 @@ interface FetchyResponse extends Response {
   text(): Promise<string>
 }
 
-const instance = (fetch: typeof window.fetch) => (verb: httpVerbs) => async (
+const attachTransformsToResponse = async (
+  _response: Response
+): Promise<FetchyResponse> => {
+  let response: FetchyResponse = _response
+  for (const methods of bodyTransform) {
+    response[methods] = await _response[methods]()
+  }
+  return response
+}
+
+const originalFetch = async (
+  _fetch: typeof window.fetch,
+  verb: httpVerbs,
+  resource: string,
+  init?: RequestInit
+) => {
+  const _init = {
+    method: verb,
+    ...init,
+  }
+  const response = await _fetch(resource, _init)
+  return attachTransformsToResponse(response)
+}
+
+const jsonBodyFirstFetch = async (
+  _fetch: typeof window.fetch,
+  verb: httpVerbs,
   resource: string,
   body?: Json,
-  init?: Omit<RequestInit, 'body'>
-): Promise<FetchyResponse> => {
+  init?: RequestInit
+) => {
   const _init: RequestInit = {
     method: verb,
-    ...(body && isHeadOrGet(verb) && { body: JSON.stringify(body) }),
+    ...(body && { body: JSON.stringify(body) }),
     ...(init && init),
   }
-  const _response = await fetch(resource, _init)
-  let result: FetchyResponse = _response
-  for (const methods of bodyTransform) {
-    result[methods] = await _response[methods]()
+
+  const response = await _fetch(resource, _init)
+  return attachTransformsToResponse(response)
+}
+
+function instance(
+  _fetch: typeof window.fetch,
+  verb: 'GET' | 'HEAD'
+): (resource: string, init?: RequestInit) => Promise<FetchyResponse>
+function instance(
+  _fetch: typeof window.fetch,
+  verb: 'POST' | 'PUT' | 'DELETE' | 'PATCH'
+): (
+  resource: string,
+  body?: Json,
+  init?: RequestInit
+) => Promise<FetchyResponse>
+function instance(_fetch: typeof window.fetch, verb: httpVerbs) {
+  if (isHeadOrGet(verb)) {
+    return originalFetch.bind(null, _fetch, verb)
   }
-  return result
+  return jsonBodyFirstFetch.bind(null, _fetch, verb)
 }
 
-const defaultInstance = instance(fetch)
-
-export default {
-  get: defaultInstance('GET'),
-  post: defaultInstance('POST'),
-  put: defaultInstance('PUT'),
-  delete: defaultInstance('DELETE'),
-  patch: defaultInstance('PATCH'),
-  head: defaultInstance('HEAD'),
+function init() {
+  return {
+    get: instance(fetch, 'GET'),
+    post: instance(fetch, 'POST'),
+    put: instance(fetch, 'PUT'),
+    delete: instance(fetch, 'DELETE'),
+    patch: instance(fetch, 'PATCH'),
+    head: instance(fetch, 'HEAD'),
+  }
 }
 
-/*
-    1. req.get
-    2. req.post
-    3. req.put
-    4. req.patch
-    5. req.head
-    6. req.delete
-    7. req.createInstance(baseurl, interceptors, timeout)
-    8. req.extendInstance(baseurl, interceptors, timeout)
-    9. return cancel to kill it?
- */
+export default init()
